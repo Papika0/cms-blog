644f6c41abbf008eecb34913b21e56cd808a9ed6
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-02-24 16:53:46 +0400

    Postcard jsx done

---
+++ services/index.js
@@ -0,0 +1,38 @@
+import { graphql } from "graphql";
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHQL_API;
+
+export const getPosts = async () => {
+  const query = gql`
+    query Assets {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
