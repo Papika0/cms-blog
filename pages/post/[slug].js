aec07cc84932214825d44651beba12504e6b0ca8
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-02-26 15:34:01 +0400

    project done, without comments

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 import { getPosts, getPostDetails } from "../../services";
 
@@ -9,17 +10,22 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from "../../components";
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <Loader />;
+  }
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           <PostDetail post={post} />
           <Author author={post.author} />
-          <CommentsForm slug={post.slug} />
-          <Comments slug={post.slug} />
+          {/* <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} /> */}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -50,6 +56,6 @@ export async function getStaticPaths() {
   const posts = await getPosts();
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })) || [],
-    fallback: false,
+    fallback: true,
   };
 }
